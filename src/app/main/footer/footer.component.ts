6af3aec15f9eed8da142368a4fb78c7ae35b051b
ArjKre <crkreshnan916@gmail>
2024-08-29 02:38:34 +0530

    V5 | Footer Section 100%

--- src/app/main/footer/footer.component.ts
+++ src/app/main/footer/footer.component.ts
@@ -8,9 +8,16 @@ import { Component, ElementRef, Output, ViewChild, EventEmitter, AfterViewInit }
 export class FooterComponent implements AfterViewInit {
   
   email = 'arj@kr3.dev';
+  emailLink = `mailto:${this.email}`;
   isPaused = false;
 
   @ViewChild('futContainer',{static: true}) futContainer! : ElementRef<HTMLElement>;
+  
+  @ViewChild('angLink',{static: true}) angularLink! : ElementRef<HTMLElement>;
+
+  @ViewChild('gitLink',{static: true}) githubLink! : ElementRef<HTMLElement>;
+  
+  @ViewChild('linkedinLink',{static: true}) linkedinLink! : ElementRef<HTMLElement>;
 
   @Output() footerHeightEmitter =  new EventEmitter<HTMLElement>();
 
@@ -26,5 +33,31 @@ export class FooterComponent implements AfterViewInit {
 
   resumeAnimation(): void {
     this.isPaused = false;
+  }
+
+  angularMouseOver(){
+    this.angularLink.nativeElement.style.filter = 'grayscale(0)';
+    this.angularLink.nativeElement.style.opacity = '1';
+  }
+
+  angularMouseLeave(){
+    this.angularLink.nativeElement.style.filter = 'grayscale(0.3)';
+    this.angularLink.nativeElement.style.opacity = '0.5';
+  }
+
+  githubMouseOver(){
+    this.githubLink.nativeElement.style.opacity = '1';
+  }
+
+  githubMouseLeave(){
+    this.githubLink.nativeElement.style.opacity = '0.5';
+  }
+
+  linkedinMouseOver(){
+    this.linkedinLink.nativeElement.style.opacity = '1';
+  }
+
+  linkedinMouseLeave(){
+    this.linkedinLink.nativeElement.style.opacity = '0.5';
   }
 }
