072f1156afa27c5b05ef9f4798ca5d315ee55c91
Arjun Kreshnan <crkreshnan916@gmail.com>
2026-04-16 13:12:27 +0530

    updated resume pdf

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -10,25 +10,25 @@ export class AboutComponent implements OnInit {
   animationState: string = 'start';
 
   @ViewChild('resume', { static: true })resume!: ElementRef<HTMLAnchorElement>;
-  
+
   constructor() {}
-  
+
   @Output() backBtn = new EventEmitter();
-  
+
   ngOnInit(): void {
     setTimeout(() => {
       this.animationState = 'end';
     }, 0);
   }
-  
+
   onBtnClicked() {
     this.backBtn.emit(!this.isBtnClicked);
   }
-  
+
   downloadResume() {
     const link = this.resume.nativeElement;
-    link.href = '../../../assets/resume2024.pdf';
-    link.download = 'resume2024.pdf';
+    link.href = '../../../assets/resumeApril2026.pdf';
+    link.download = 'resumeApril2026.pdf';
     window.open(link.href,'_blank');
   }
 }
